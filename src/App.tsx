e0aee96175cd03ea91cf1ffbe32695a4dc0e3b06
Vaibhav <vaibhav.m@shipmantra.com>
2024-11-07 15:05:42 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import Header from './components/Header';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+// src/components/Header.tsx
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hi, my portfolio coming soon!</h1>
+    </header>
+  );
+};
+
+export default Header;
