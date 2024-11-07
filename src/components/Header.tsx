f8db1ccabeab38003e238d70f205b8b7563f332e
vaibhavvenkat <vaibhavvenkatm@gmail.com>
2024-11-07 15:24:06 +0530

    try change git

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hi, my portfolio coming soon!</h1>
+      <h1>Hi, my portfolio coming soon! Stay Tuned.</h1>
     </header>
   );
 };
